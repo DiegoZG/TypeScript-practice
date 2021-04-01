4fbae48f08189644d5f79df8f4d25ba601ba59a8
Diwo777 <diego.zegarra79@icloud.com>
2021-04-01 17:38:53 -0400

    Tyscript Arrays

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -10,3 +10,14 @@ const myCar = carMakers.pop()
 // Prevent incompatible values
 
 carMakers.push(100)
+
+//Help with 'map'
+
+carMakers.map((car: string): string => {
+  return car
+})
+
+// flexible types
+
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10']
+importantDates.push('2020-10-10')
