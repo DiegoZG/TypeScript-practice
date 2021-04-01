99e4d3a8469af7ebbfe9d285dc7e5141cad350dd
Diwo777 <diego.zegarra79@icloud.com>
2021-04-01 17:07:01 -0400

    TS Practice

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,2 +1,12 @@
 const carMakers = ['ford','toyota', 'chevy']
-const carsByMake: string[][] = []
+const dates = [new Date(), new Date()];
+const carsByMake: string[][] = [];
+
+//help with inference when extracting values
+
+const car1 = carMakers[0]
+const myCar = carMakers.pop()
+
+// Prevent incompatible values
+
+carMakers.push(100)
